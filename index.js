8e8baaa6f53a9c4fed0355472908ac4f7566c29b
Joshua <joshua.lopez8893@yahoo.com>
2023-02-02 20:40:30 -0800

    changed border line size

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express()
 app.get('/', function (req, res) {
     res.send(`
         <body style="margin: 0;">
-            <div style="border: 1px solid black; height: 10vh; background-color: white;">
+            <div style="border: 3px solid black; height: 10vh; background-color: white;">
                 <h2 style="text-align: center;">NAV BAR</h2>
             </div>
             <h1>White Page</h1>
